1deb266d5d979dab16f4a25a1cd3521a3c85622b
Hyeji Lee <zzambbang@gmail.com>
2022-11-18 23:29:23 +0900

    fix: house service 메서드 명 수정 (#3)
    * fix: house db 필드 수정
    
    * feat: 관심 아파트 기능 추가
    
    * env: JUnit4 추가
    
    * fix: house service 메서드 명 수정

--- backend/src/main/java/com/ssafy/myhome/model/dao/InterestHouseDao.java
+++ backend/src/main/java/com/ssafy/myhome/model/dao/InterestHouseDao.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.ssafy.myhome.model.dto.HouseInfo;
+import com.ssafy.myhome.model.dto.InterestHouse;
 
 @Mapper
 public interface InterestHouseDao {
 
-	InterestHouseDao getUserInterestHouse(int interestHouseId);
-	List<InterestHouseDao> getUserInterestHouses(String userId);
-	
+	InterestHouse getUserInterestHouse(int interestHouseId);
+	List<InterestHouse> getUserInterestHouses(String userId);
 	int insertUserInterestHouseMapping(String userId, HouseInfo houseInfo);
 	int deleteUserInterestHouseMapping(int interestHouseId);
 	
