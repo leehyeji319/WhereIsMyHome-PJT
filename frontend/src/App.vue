8f6064dda7b6d4e84d45e2e0e434d6b5d2411bb2
KangHyojin1401 <khj6354@naver.com>
2022-11-22 00:57:37 +0900

    Chore: frontend vuetify 버전 추가

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,43 +1,50 @@
 <template>
-  <div id="app">
-    <the-header-navbar></the-header-navbar>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import TheHeaderNavbar from "@/components/layout/TheHeaderNavbar";
-
 export default {
   name: "App",
-  components: {
-    TheHeaderNavbar,
-  },
+
+  data: () => ({
+    //
+  }),
 };
-
 </script>
-
-<style>
-
-@import url("https://webfontworld.github.io/NanumSquareNeo/NanumSquareNeo.css");
-#app {
-  font-family: "NanumSquareNeo";
-  font-style: normal;
-  font-display: swap;
-  text-align: center;
-}
-
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,20 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
