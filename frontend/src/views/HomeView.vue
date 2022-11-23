d1bba41bea565e0672138a92001c9b523b61edc4
leehyeji319 <zzambbang@gmail.com>
2022-11-23 17:47:49 +0900

    feat: front 매물 등록 페이지

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,20 +1,23 @@
 <template>
   <v-app>
-    <board-list></board-list>
-    <board-detail></board-detail>
+    <!-- <board-list></board-list>
+    <board-detail></board-detail> -->
+    <board-register></board-register>
   </v-app>
 </template>
 
 <script>
-import BoardList from "@/components/board/BoardList.vue";
-import BoardDetail from "@/components/board/BoardDetail.vue";
+// import BoardList from "@/components/board/BoardList.vue";
+// import BoardDetail from "@/components/board/BoardDetail.vue";
+import BoardRegister from "@/components/board/BoardRegister";
 
 export default {
   name: "HomeView",
 
   components: {
-    BoardList,
-    BoardDetail,
+    // BoardList,
+    // BoardDetail,
+    BoardRegister,
   },
 };
 </script>
