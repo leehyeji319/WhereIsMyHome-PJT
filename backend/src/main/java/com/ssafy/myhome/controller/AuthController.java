23bdd6e5013f1fb9ae60d10c4fb23a2f49476b77
Hyeji Lee <zzambbang@gmail.com>
2023-01-27 15:38:26 +0900

    Update AuthController.java

--- backend/src/main/java/com/ssafy/myhome/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/myhome/controller/AuthController.java
@@ -70,7 +70,6 @@ public class AuthController {
 		return ResponseEntity.ok().build();
 	}
 	
-	//==효인쌤 ==//
 	@PostMapping("/login")
 	public ResponseEntity<Map<String, Object>> login(
 			@RequestBody @ApiParam(value = "로그인 시 필요한 회원정보(아이디, 비밀번호).", required = true) UserInfo userInfo) throws Exception {
